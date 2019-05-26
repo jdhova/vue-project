c6b60a577e58f7c72b0c49203a2dd1e4bc6095dd
Jude OkAGU <juud0010@gmail.com>
2019-05-26 14:58:20 +0200

    test

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <h1>{{ msg }}</h1>
+    <h3> {{info}} </h3>
+    
+    
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name:'app',
+    data(){
+      return {
+        info: 'lets see whats here',
+        msg: 'Welcome to Your Vue.js App jude is coding'
+      }
+    }
+  }
+ </script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
